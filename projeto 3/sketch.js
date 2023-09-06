835be7c548400985c5fa48c50d86483422ecf5ad
samuelbernardinidasil <138732098+samuelbernardinidasil@users.noreply.github.com>
2023-09-06 20:17:19 -0300

    Add files via upload

---
+++ projeto 3/sketch.js
@@ -0,0 +1,39 @@
+let cor;
+let posicaoHrizontal; // x
+let posicaoVertical; // y
+function setup() {
+
+  createCanvas(400, 400);
+background(color(100, 0, 0));
+cor = color(random(0, 255), random(0, 255), random(0, 255));
+}
+posicaoHorizontal = 200;
+posicaoVertical = 200;
+
+
+function draw() {
+ 
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  fill(cor);
+  
+  if(mouseX < posicaoHorizontal) {
+ posicaoHorizontal = posicaoHorizontal - 1;
+  }
+    
+  
+  if(mouseX > posicaoHorizontal)
+  posicaoHorizontal = posicaoHorizontal + 1;
+  
+  
+  if(mouseY < posicaoVertical)
+  posicaoVertical--;
+  
+  if(mouseY > posicaoVertical)
+  posicaoVertical++;
+  
+  
+  if(mouseIsPressed);
+  
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  
+}
